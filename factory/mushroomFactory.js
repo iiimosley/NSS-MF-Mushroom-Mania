afb464c214c7a10d280d98a54847439647d490e9
Trey Mosley <iiimosley@Treys-MacBook-Pro.local>
2018-01-26 13:27:48 -0600

    firebase url update

--- factory/mushroomFactory.js
+++ factory/mushroomFactory.js
@@ -5,7 +5,7 @@ angular.module("mushroomMania").factory("MushroomFactory", function ($q, $http) 
     let getMushrooms = () => {
         return $q((resolve, reject) => {
             $http
-                .get("https://tm-nss-mushroommania.firebaseio.com/mushrooms.json")
+                .get("https://tm-nss-jan8.firebaseio.com/mushrooms.json")
                 .then((mushrooms) => {
                     let allShrooms = Object.values(mushrooms.data)
                     resolve(allShrooms);
